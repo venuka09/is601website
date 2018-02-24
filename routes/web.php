f9ba982d8314859475f89b36f6f9b214c9f16dfb
venuka <vv356@njit.edu>
2018-02-24 17:15:04 -0500

    added about, contact and submit

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,18 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
+});
+Route::get('/about', function () {
+    return view('pages.about');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+Route::post('/contact', function () {
+
+    $data = request() -> all();
+
+    echo " Email: " . $data['email'] . "<br>";
+    echo " Body: " . $data['body'];
 });
