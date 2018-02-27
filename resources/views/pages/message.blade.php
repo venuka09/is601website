9e70f8242d4b053da56e3c42648d33a4492e6b82
venuka <vv356@njit.edu>
2018-02-26 20:38:02 -0500

    added card on static message page

--- resources/views/pages/message.blade.php
+++ resources/views/pages/message.blade.php
@@ -2,7 +2,13 @@
 
 @section('content')
 
-
-        <p class="lead">Please let me know how to solve my problem?</p>
-
+    <div class="card">
+        <div class="card-header"><h1>Message:</h1>
+        </div>
+        <div class="card-body">
+            <p class="lead">Subject: Need information</p>
+            <p class="lead">From Email: abcdef@gmail.com</p>
+            <p class="lead">Message: Please let me know how to solve my problem?</p>
+        </div>
+    </div>
 @endsection
